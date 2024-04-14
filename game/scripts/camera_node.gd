d8a5d33e5c8c072c2e3261461ce1da10d9c3bd6e
cbwhickstein <contact@christian-hickstein.com>
2024-04-14 09:19:44 +0200

    finished hearts

--- game/scripts/camera_node.gd
+++ game/scripts/camera_node.gd
@@ -5,14 +5,15 @@ var player: CharacterBody2D = null
 var y_offset = 125
 var life_points = 0
 var life_points_sprites = []
-var initial_heart_position = Vector2(100, -100)
-var heart_position_offset = Vector2(100, 0)
+var initial_heart_position = Vector2(225, -150)
+var heart_position_offset = Vector2(25, 0)
 
 func _ready():
 	player = get_node("../Player")
 	if (player == null):
 		print("Player could not connect to camera node")
 	else:
+		player.connect("player_hurt", _on_player_hurt)
 		position.x = player.x_position_multiplier
 		life_points = player.life_points
 		
@@ -20,7 +21,7 @@ func _ready():
 			life_points_sprites.push_front(Sprite2D.new())
 			life_points_sprites[0].texture = preload("res://assets/Collectables/heart.png")
 			life_points_sprites[0].set_texture_filter(TEXTURE_FILTER_NEAREST)
-			life_points_sprites[0].scale * 10
+			life_points_sprites[0].scale *= 2
 			
 			if (life_points_sprites.size() == 1):
 				life_points_sprites[0].position = initial_heart_position
@@ -34,3 +35,11 @@ func _ready():
 func _process(delta):
 	if (player != null):
 		position.y = player.position.y - y_offset
+		
+		
+		
+func _on_player_hurt(_collider):
+	life_points -= 1
+	var heart = life_points_sprites.pop_back()
+	remove_child(heart)
+	heart.queue_free()
